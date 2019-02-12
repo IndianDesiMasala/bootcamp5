2150d5e8aee86e61895093962242c2a889e94c59
IndianDesiMasala <32110532+IndianDesiMasala@users.noreply.github.com>
2019-02-11 20:35:47 -0500

    Add files via upload

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -1,14 +1,14 @@
 
 /* Dependencies */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Listing = require('../models/listings.server.model.js');
 
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
-  On an error you should send a 404 status code, as well as the error message. 
+  On an error you should send a 404 status code, as well as the error message.
   On success (aka no error), you should send the listing(s) as JSON in the response.
 
-  HINT: if you are struggling with implementing these functions, refer back to this tutorial 
+  HINT: if you are struggling with implementing these functions, refer back to this tutorial
   from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
  */
 
@@ -43,11 +43,28 @@ exports.update = function(req, res) {
   /** TODO **/
   /* Replace the article's properties with the new properties found in req.body */
   /* Save the article */
+  Object.assign(listing, req.body);
+  listing.save(function(err) {
+    if(err) {
+      console.log(err);
+      res.status(404).send(err);
+    } else {
+      res.json(listing);
+    }
+  });
 };
 
 /* Delete a listing */
 exports.delete = function(req, res) {
   var listing = req.listing;
+  listing.remove(function(err){
+    if(err){
+      console.log(err);
+      res.status(404).send(err);
+    } else {
+      res.sendStatus(200);
+    }
+  });
 
   /** TODO **/
   /* Remove the article */
@@ -57,13 +74,28 @@ exports.delete = function(req, res) {
 exports.list = function(req, res) {
   /** TODO **/
   /* Your code here */
+  Listing.find({}, function(err,listings){
+    if(err){
+      console.log(err);
+      res.status(404).send(err);
+      return;
+    }
+    listings.sort(function(a, b){
+      if (a.code < b.code)
+        return -1;
+      if(b.code < a.code)
+        return 1;
+      return 0;
+    });
+    res.send(listings);
+  })
 };
 
-/* 
-  Middleware: find a listing by its ID, then pass it to the next request handler. 
+/*
+  Middleware: find a listing by its ID, then pass it to the next request handler.
 
-  Find the listing using a mongoose query, 
-        bind it to the request object as the property 'listing', 
+  Find the listing using a mongoose query,
+        bind it to the request object as the property 'listing',
         then finally call next
  */
 exports.listingByID = function(req, res, next, id) {
