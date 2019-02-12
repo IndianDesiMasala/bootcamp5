7cb1ed811b4fb65c3ffbb25f8c44f277c31d7504
IndianDesiMasala <32110532+IndianDesiMasala@users.noreply.github.com>
2019-02-11 23:02:24 -0500

    Update listingFactory.js

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://bootcampassignment5.herokuapp.com/api/listings');
+      return $http.get('http://bootcampassignment5.herokuapp.com/api/listings');
     },
 
 	create: function(listing) {
-	  return $http.post('https://bootcampassignment5.herokuapp.com/api/listings', listing);
+	  return $http.post('http://bootcampassignment5.herokuapp.com/api/listings', listing);
     },
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-       return $http.delete('https://bootcampassignment5.herokuapp.com/api/listings' + id);
+       return $http.delete('http://bootcampassignment5.herokuapp.com/api/listings' + id);
 
     }
   };
